8fe57804736ce763c87e7ddc2589f36acad6bf08
soumya suansia <ssuansia@cisco.com>
2016-07-29 17:34:06 +0530

    Added print statements

--- python/src/p12_HighlyDivisibleTriangularNumber_v1.py
+++ python/src/p12_HighlyDivisibleTriangularNumber_v1.py
@@ -1,13 +1,18 @@
 import time
+from time import sleep
  
 def num_divisors(n):
+    # n = 10
     if n % 2 == 0: n = n/2
+    # n = 5
     divisors = 1
     count = 0
     while n % 2 == 0:
         count += 1
         n = n/2
+    # n = 1
     divisors = divisors * (count + 1)
+    # divisor = 1
     p = 3
     while n != 1:
         count = 0
@@ -21,13 +26,21 @@ def num_divisors(n):
 def find_triangular_index(factor_limit):
     n = 1
     lnum, rnum = num_divisors(n), num_divisors(n+1)
+    print "lnum: " + str(lnum) + " rum: " + str(rnum)
+    print "***********>>>>>>>><<<<<<<**********"
+    sleep(1)
     while lnum * rnum < 500:
-        n += 1
+        n += 1        
+        print "Round: " +  str(n)        
         lnum, rnum = rnum, num_divisors(n+1)
+        print "lnum: " + str(lnum) + " rum: " + str(rnum)
+        sleep(1)
     return n
  
 start = time.time()
 index = find_triangular_index(500)
+print "The index: " + str(index)
+sleep(1)
 triangle = (index * (index + 1)) / 2
 elapsed = (time.time() - start)
  
